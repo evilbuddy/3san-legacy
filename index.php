ed452a719143524d9cb60088a3bee287a043ed1a
Evil Buddy <evilbuddy74@gmail.com>
2025-01-14 19:59:48 +0000

    Added rules

--- index.php
+++ index.php
@@ -25,6 +25,28 @@ require("db.php");
 
 	<div class="window">
 		<div class="title">
+			<h1>Rules</h1>
+		</div>
+		<ol>
+			<li>You will not upload, post, discuss, request, or link to anything that violates local or United States law.</li>
+			<li>You will immediately cease access to the site if you are under the age of 18.</li>
+			<li>You will not post or request personal information ("dox") or calls to invasion ("raids").</li>
+			<li>Advertising is strictly prohibited.</li>
+			<li>Do not upload images containing additional data such as embedded sounds, documents, archives, etc.</li>
+			<li>Linking to possible shocking or malicious content will not be tolerated.</li>
+			<li>Keep all posts, uploads and discussions in touch with the board's subject.</li>
+			<li>Do not post any content that can be considered Not Safe For Work ("NSFW") outside of boards in the NSFW category.</li>
+			<li>Do not post any of the following: <ul>
+				<li>Troll posts</li>
+				<li>Racism</li>
+				<li>Grotesque (Guro, Gore) images</li>
+				<li>Dubs or GET posts, including "Roll for X" images</li>
+			</ul></li>
+		</ol>
+	</div>
+
+	<div class="window">
+		<div class="title">
 			<h1>Boards</h1>
 		</div>
 		<table id="boards"><tr>
